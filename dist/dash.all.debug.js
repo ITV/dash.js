e4f0af497e001428e0957737195db889346c1e0e
Albert Bosch <albert@intrepidloop.com>
2016-10-21 11:22:19 +0100

    MP-1579 - new build artifacts after upgrading Dash to v2.3.0

--- dist/dash.all.debug.js
+++ dist/dash.all.debug.js
@@ -27346,7 +27346,7 @@ function ProtectionKeyController() {
         try {
             return clearkeyKeySystem.getClearKeysFromProtectionData(protData, message);
         } catch (error) {
-            log('Failed to retrieve clearkeys from ProtectionData');
+            log('Failed to retrieve clearkeys from ProtectionData: ' + error);
             return null;
         }
     }
@@ -29170,19 +29170,12 @@ function ClearKey() {
 
     var instance = undefined;
 
-    function getServerURLFromMessage(url, message /*, messageType*/) {
-        // Build ClearKey server query string
-        var jsonMsg = JSON.parse(String.fromCharCode.apply(null, new Uint8Array(message)));
-        url += '/?';
-        for (var i = 0; i < jsonMsg.kids.length; i++) {
-            url += jsonMsg.kids[i] + '&';
-        }
-        url = url.substring(0, url.length - 1);
+    function getServerURLFromMessage(url /*, messageType*/) {
         return url;
     }
 
     function getHTTPMethod() /*messageType*/{
-        return 'GET';
+        return 'POST';
     }
 
     function getResponseType() /*keySystemStr*/{
